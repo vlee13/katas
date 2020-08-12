c7ba20ec3b8b8101d07c50e9d061fd9d893f572f
vlee13 <vlee13@gmail.com>
2020-08-11 23:25:20 -0700

    3rd katas

---
+++ syntaxDebug.js
@@ -0,0 +1,12 @@
+`Grasshopper - Function syntax debugging
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.`;
+
+//incorrect syntax for function
+`function main [verb, noun]
+  return verb + noun
+}`;
+
+//correct syntax for function
+function main(verb, noun) {
+  return verb + noun;
+}
