ddc04bda16a7b1bde3beedf009fa2c0f88b98cef
vlee13 <vlee13@gmail.com>
2020-08-26 15:07:42 -0700

    add kata

--- 7kyu/friendOrFoe.js
+++ 7kyu/friendOrFoe.js
@@ -1,8 +1,10 @@
-`Make a program that filters a list of strings and returns a list with only your friends name in it.
+// `Make a program that filters a list of strings and returns a list with only your friends name in it.
 
-If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
 
-Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]`;
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]`;
+
+// `https://www.codewars.com/kata/55b42574ff091733d900002f`
 
 //solution 1 with for loop
 function friend(friends) {
