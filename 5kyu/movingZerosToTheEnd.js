249d48a4cb0f71eba674a4761b01287a41b22e1f
vlee13 <vlee13@gmail.com>
2021-05-18 14:04:14 -0400

    new katas

--- 5kyu/movingZerosToTheEnd.js
+++ 5kyu/movingZerosToTheEnd.js
@@ -5,7 +5,7 @@
 //https://www.codewars.com/kata/52597aa56021e91c93000cb0
 
 //Solution 1
-var moveZeros = function (arr) {
+const moveZeros = function (arr) {
   let arr1 = [];
   let arr2 = [];
   for (let i = 0; i < arr.length; i++) {
@@ -20,7 +20,8 @@ var moveZeros = function (arr) {
 
 //Solution 2
 const moveZeros = (arr) => {
-  for (var i = arr.length - 1; i >= 0; i--) {
+  //loop has to be backwards bc of splice takes out element and rest of elements shift to the left
+  for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] === 0) {
       arr.splice(i, 1);
       arr.push(0);
@@ -30,5 +31,12 @@ const moveZeros = (arr) => {
 };
 
 //Solution 3
+var moveZeros = function (arr) {
+  let newArr = arr.filter((ele) => ele !== 0);
+  let zeroArr = arr.filter((ele) => ele === 0);
+  return newArr.concat(zeroArr);
+};
+
+//Solution 3 refactored to 1 line
 const moveZeros = (arr) =>
-  arr.filter((i) => i !== 0).concat(arr.filter((n) => n === 0));
+  arr.filter((el) => el !== 0).concat(arr.filter((el) => el === 0));
---
+++ 6kyu/Array.diff.js
@@ -0,0 +1,22 @@
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+//Solution 1
+function arrayDiff(a, b) {
+  let newArr = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!b.includes(a[i])) {
+      newArr.push(a[i]);
+    }
+  }
+  return newArr;
+}
+
+//Solution 1 refactored to 1 line
+const arrayDiff = (a, b) => a.filter((el) => !b.includes(el));
