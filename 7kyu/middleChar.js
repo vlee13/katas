b76cb9fd837359c7656822bb514113512b4639c7
vlee13 <vlee13@gmail.com>
2021-06-30 10:06:55 -0400

    update

--- 7kyu/middleChar.js
+++ 7kyu/middleChar.js
@@ -1,12 +1,14 @@
-`You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.``#Examples:
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.``#Examples:
 
-Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("test") should return "es"
 
-Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("testing") should return "t"
 
-Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("middle") should return "dd"
 
-Kata.getMiddle("A") should return "A"`;
+// Kata.getMiddle("A") should return "A"
+
+// https://www.codewars.com/kata/56747fd5cb988479af000028
 
 //Solution 1
 function getMiddle(s) {
@@ -20,4 +22,21 @@ function getMiddle(s) {
     length = 1;
   }
   return s.substring(position, position + length);
+}
+
+//Solution 2
+function getMiddle(s) {
+  let x = Math.floor(s.length / 2);
+  return s.length % 2 === 0 ? s[x - 1] + s[x] : s[x];
+  // if(s.length % 2 == 0){
+  //   return s[ x - 1] + s[x]
+  // } else {
+  //   return s[x]
+  // }
+
+  // if(s.length % 2 === 0){
+  //   return s.charAt(s.length/2-1) + s.charAt(s.length/2)
+  // }else{
+  //   return s.charAt(s.length/2)
+  // }
 }
